8796c09bfd1d26919d703bac32da90b3f6eb595e
Richard Laskey <rlaskey@outlook.com>
2026-01-31 10:23:03 -0500

    reduce iOS zooming.

--- app/layer/app.tsx
+++ app/layer/app.tsx
@@ -4,7 +4,7 @@ export const App = (
   <html lang="en">
     <head>
       <meta charset="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <title>{props.title}</title>
       {props.scripts.map((e) => (
         <script key={e} src={e} type="module">
